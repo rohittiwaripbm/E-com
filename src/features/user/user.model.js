e488b9c7739d08722ea297c6a08b11075402b9c3
Rohit Tiwari <rohittiwaripbm@gmail.com>
2024-03-31 23:52:13 +0530

    Repository pattern added

--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -10,33 +10,6 @@ export default class UserModel{
         this.password = password;
         this.type = type;
     }
-
-    static signIn(email, password)
-    {
-        console.log(email)
-        let user = users.find(u=>u.email==email && u.password ==  password);
-
-        return user;
-    }
-
-    static async signUp(name, email, password, type)
-    {
-        try {
-            //1. Get the database
-            const db = getDB();
-
-            //2. Get the collection
-            const collection = db.collection("users");
-            let newUser = new UserModel(name, email, password, type);
-            await collection.insertOne(newUser);
-            return newUser;
-
-
-            
-        } catch (error) {
-            throw new customErrorHandler(500, "Something is not working fine");
-        }
-    }
     static getAll()
     {
         return users;
